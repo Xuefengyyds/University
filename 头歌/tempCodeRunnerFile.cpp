a2ad74012a400b61b0667055e26f1d75037a40d0
Krive <1784195276@qq.com>
2023-12-13 22:55:45 +0800

    init

---
+++ 头歌/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+int z ,b=-1,c=20;
+    z=++b||(c%=3);
+    cout<<z<<b<<c;
+    return 0;
---
+++ 头歌/test.cpp
@@ -0,0 +1,10 @@
+  #include<iostream>
+    using namespace std;
+    int main(void)
+    {
+       int a=1,b=0; 
+       if(a!=1)cout<<1<<endl;
+       else if(b=0)cout<<2<<endl;        //把1赋值给b，条件为真
+       else cout<<3<<endl;
+       return 0;
+    }
