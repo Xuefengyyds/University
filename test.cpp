56971001e6153f3eac103520177731a8967848c5
mac <1784195276@qq.com>
2024-01-05 20:04:30 +0800

    优化17之前

--- test.cpp
+++ test.cpp
@@ -2,23 +2,10 @@
 #include<iostream>
 #include <cstring>
 using namespace std;
-char* f(char *s);   // 将指针s所指字符数组逆序后返回
 int main(void)
 {   
-    char str[100];
-    int i,j;
-    cin.getline(str,100);
-    char *p1,*p2;
-     for(i=0,j=strlen(str)-1;i<strlen(str)/2;i++,j--)
-    {      
-        p1=&str[i];
-        p2=&str[j];
-        char temp;
-        temp=*p2;
-        *p2=*p1;
-        *p1=temp;
-    }
-    cout<<str;
-    system("pause");
+char s[10]="abced";
+cin>>s;
+cout<<s;
     return 0;
 }
