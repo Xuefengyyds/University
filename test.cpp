86f5f4b20fadf7ee44b811d12aa2d82b83ad756e
Krive <1784195276@qq.com>
2024-01-06 23:28:20 +0800

    完成单元测试4的解析

--- test.cpp
+++ test.cpp
@@ -1,11 +1,24 @@
-// 设计程序，将键盘输入的字符串逆序。
 #include<iostream>
-#include <cstring>
-using namespace std;
-int main(void)
-{   
-char s[10]="abced";
-cin>>s;
-cout<<s;
-    return 0;
-}
+   using namespace std;
+   class MyClassA{
+    protected:
+       int a;
+   public:
+       MyClassA( ) { a=1;}
+       void print(){cout<<a;}
+   };
+   class MyClassB:public MyClassA{
+       int a;
+   public:
+       MyClassB( )  { a=2;}
+       void print(){cout<<MyClassA::a<<a;}
+   };
+   int main()
+   {
+       MyClassB t;                //调用了构造函数，但先把2赋值给类自身的a
+       MyClassA *p=&t;            //赋值的兼容性，访问的是A中的print函数
+       p->print();
+       t.print();
+       system("pause");
+       return 0;
+   }
