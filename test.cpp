ace815abe009c2dbaf631cde78ae8ea245fced37
Krive <1784195276@qq.com>
2024-01-08 17:20:13 +0800

    提交

--- test.cpp
+++ test.cpp
@@ -1,24 +1,12 @@
 #include<iostream>
-   using namespace std;
-   class MyClassA{
-    protected:
-       int a;
-   public:
-       MyClassA( ) { a=1;}
-       void print(){cout<<a;}
-   };
-   class MyClassB:public MyClassA{
-       int a;
-   public:
-       MyClassB( )  { a=2;}
-       void print(){cout<<MyClassA::a<<a;}
-   };
-   int main()
-   {
-       MyClassB t;                //调用了构造函数，但先把2赋值给类自身的a
-       MyClassA *p=&t;            //赋值的兼容性，访问的是A中的print函数
-       p->print();
-       t.print();
-       system("pause");
-       return 0;
-   }
+using namespace std;
+int main(){
+    double a[]={1,3,5,7,9};  
+
+    // cout<<&a[0]<<'\t'<<&a[1];
+
+    cout<<&a[3]-&a[2]<<endl;
+
+
+    return 0;
+}
